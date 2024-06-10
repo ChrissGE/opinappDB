5d626866d8f9f6ad0f38500705d79a0c9597cf9b
PC_Casa\chris <chrisgaes07@gmail.com>
2024-06-10 17:28:29 +0200

    no message

--- Server.py
+++ Server.py
@@ -312,86 +312,137 @@ def getQuestionary():
         name_language=datos.get('name_language')
 
         if name_language.lower()=='español':
-            language_code="ES"
+            if company_code =="U31477490":
+                results = [
+                                {
+                                    "id_questions": 13,
+                                    "question_text": "¿Consideras que la comida estaba fresca y bien preparada durante tu visita?",
+                                    "menu_text": "Calidad de la Comida",
+                                    "question_type": "Yes and no",
+                                    "company_name": "Ginos"
+                                },
+                                {
+                                    "id_questions": 14,
+                                    "question_text": "¿Te pareció que los ingredientes utilizados en los platos eran de alta calidad y sabrosos?",
+                                    "menu_text": "Calidad de la Comida",
+                                    "question_type": "Yes and no",
+                                    "company_name": "Ginos"
+                                },
+                                {
+                                    "id_questions": 15,
+                                    "question_text": "¿Sentiste que la atmósfera del restaurante era acogedora y agradable?",
+                                    "menu_text": "Experiencia en el Restaurante",
+                                    "question_type": "Yes and no",
+                                    "company_name": "Ginos"
+                                },
+                                {
+                                    "id_questions": 16,
+                                    "question_text": "¿La presentación de los platos y la decoración general del restaurante contribuyeron positivamente a tu experiencia?",
+                                    "menu_text": "Experiencia en el Restaurante",
+                                    "question_type": "Yes and no",
+                                    "company_name": "Ginos"
+                                },
+                                {
+                                    "id_questions": 17,
+                                    "question_text": "¿Crees que el precio de los platos estaba justificado por la calidad y cantidad de comida que recibiste?",
+                                    "menu_text": "Valor por el Dinero",
+                                    "question_type": "Yes and no",
+                                    "company_name": "Ginos"
+                                },
+                                {
+                                    "id_questions": 18,
+                                    "question_text": "¿Consideras que la relación calidad-precio en Ginos es favorable comparada con otros restaurantes de comida italiana en la zona?",
+                                    "menu_text": "Valor por el Dinero",
+                                    "question_type": "Yes and no",
+                                    "company_name": "Ginos"
+                                },
+                                {
+                                    "id_questions": 19,
+                                    "question_text": "¿Hay algún comentario adicional que te gustaría compartir sobre tu experiencia con el servicio del personal en Ginos Restaurante?",
+                                    "menu_text": "Servicio del Personal",
+                                    "question_type": "Text",
+                                    "company_name": "Ginos"
+                                }
+                            ]
         else:
             language_code="EN"
 
         cursor = conn.cursor()
         try:
-            sql_query = f"""
-              use opinapp;
-                WITH RECURSIVE Language AS (
-                    SELECT 'ES' AS language_code
-                ),
-                TextQuestions AS (
-                    SELECT 
-                        mtq.id_questions, 
-                        qt.id_text, 
-                        qt.text,
-                        ROW_NUMBER() OVER (PARTITION BY mtq.id_questions ORDER BY 
-                            CASE 
-                                WHEN l.name_language = Language.language_code THEN 1
-                                ELSE 2
-                            END) AS rn
-                    FROM mapTextQuestions mtq
-                    LEFT JOIN texts qt ON mtq.id_text = qt.id_text
-                    LEFT JOIN languages l ON qt.id_language = l.id_language
-                    CROSS JOIN Language
-                ),
-                FilteredTextQuestions AS (
-                    SELECT id_questions, id_text, text
-                    FROM TextQuestions
-                    WHERE rn = 1
-                ),
-                TextMenus AS (
-                    SELECT 
-                        mtm.id_questionaryMenu, 
-                        tm.id_text, 
-                        tm.text,
-                        ROW_NUMBER() OVER (PARTITION BY mtm.id_questionaryMenu ORDER BY 
-                            CASE 
-                                WHEN l.name_language = Language.language_code THEN 1
-                                ELSE 2
-                            END) AS rn
-                    FROM mapTextMenu mtm
-                    LEFT JOIN texts tm ON mtm.id_text = tm.id_text
-                    LEFT JOIN languages l ON tm.id_language = l.id_language
-                    CROSS JOIN Language
-                ),
-                FilteredTextMenus AS (
-                    SELECT id_questionaryMenu, id_text, text
-                    FROM TextMenus
-                    WHERE rn = 1
-                )
-                SELECT 
-                    q.id_questions, 
-                    COALESCE(ftq.text, qt_default.text) AS question_text, 
-                    COALESCE(ftm.text, tm_default.text) AS menu_text,
-                    q.question_type,
-                    c.company_name
-                FROM questions q
-                INNER JOIN mapQuestions mq ON q.id_questions = mq.id_questions
-                INNER JOIN FilteredTextQuestions ftq ON mq.id_questions = ftq.id_questions
-                INNER JOIN questionaryMenu qm ON mq.id_questionaryMenu = qm.id_questionaryMenu
-                INNER JOIN FilteredTextMenus ftm ON qm.id_questionaryMenu = ftm.id_questionaryMenu
-                LEFT JOIN texts qt_default ON ftq.id_text = qt_default.id_text
-                LEFT JOIN texts tm_default ON ftm.id_text = tm_default.id_text
-                INNER JOIN questionaries qn ON qn.id_questionary = qm.id_questionary
-                INNER JOIN company c ON qn.company_code = c.company_code
-                WHERE qn.company_code = 'U31477490';
+        #     sql_query = f"""
+        #       use opinapp;
+        #         WITH RECURSIVE Language AS (
+        #             SELECT 'ES' AS language_code
+        #         ),
+        #         TextQuestions AS (
+        #             SELECT 
+        #                 mtq.id_questions, 
+        #                 qt.id_text, 
+        #                 qt.text,
+        #                 ROW_NUMBER() OVER (PARTITION BY mtq.id_questions ORDER BY 
+        #                     CASE 
+        #                         WHEN l.name_language = Language.language_code THEN 1
+        #                         ELSE 2
+        #                     END) AS rn
+        #             FROM mapTextQuestions mtq
+        #             LEFT JOIN texts qt ON mtq.id_text = qt.id_text
+        #             LEFT JOIN languages l ON qt.id_language = l.id_language
+        #             CROSS JOIN Language
+        #         ),
+        #         FilteredTextQuestions AS (
+        #             SELECT id_questions, id_text, text
+        #             FROM TextQuestions
+        #             WHERE rn = 1
+        #         ),
+        #         TextMenus AS (
+        #             SELECT 
+        #                 mtm.id_questionaryMenu, 
+        #                 tm.id_text, 
+        #                 tm.text,
+        #                 ROW_NUMBER() OVER (PARTITION BY mtm.id_questionaryMenu ORDER BY 
+        #                     CASE 
+        #                         WHEN l.name_language = Language.language_code THEN 1
+        #                         ELSE 2
+        #                     END) AS rn
+        #             FROM mapTextMenu mtm
+        #             LEFT JOIN texts tm ON mtm.id_text = tm.id_text
+        #             LEFT JOIN languages l ON tm.id_language = l.id_language
+        #             CROSS JOIN Language
+        #         ),
+        #         FilteredTextMenus AS (
+        #             SELECT id_questionaryMenu, id_text, text
+        #             FROM TextMenus
+        #             WHERE rn = 1
+        #         )
+        #         SELECT 
+        #             q.id_questions, 
+        #             COALESCE(ftq.text, qt_default.text) AS question_text, 
+        #             COALESCE(ftm.text, tm_default.text) AS menu_text,
+        #             q.question_type,
+        #             c.company_name
+        #         FROM questions q
+        #         INNER JOIN mapQuestions mq ON q.id_questions = mq.id_questions
+        #         INNER JOIN FilteredTextQuestions ftq ON mq.id_questions = ftq.id_questions
+        #         INNER JOIN questionaryMenu qm ON mq.id_questionaryMenu = qm.id_questionaryMenu
+        #         INNER JOIN FilteredTextMenus ftm ON qm.id_questionaryMenu = ftm.id_questionaryMenu
+        #         LEFT JOIN texts qt_default ON ftq.id_text = qt_default.id_text
+        #         LEFT JOIN texts tm_default ON ftm.id_text = tm_default.id_text
+        #         INNER JOIN questionaries qn ON qn.id_questionary = qm.id_questionary
+        #         INNER JOIN company c ON qn.company_code = c.company_code
+        #         WHERE qn.company_code = 'U31477490';
 
-            """
-            cursor.execute(sql_query)
-            print(language_code,company_code)
-            rows = cursor.fetchall()
-            print(rows)
-            if not rows:
-                return jsonify({"error": sql_query}), 500
-            results = [
-                {description[0]: row[i] for i, description in enumerate(cursor.description)}for row in rows
-            ]
+        #     """
+        #     cursor.execute(sql_query)
+        #     print(language_code,company_code)
+        #     rows = cursor.fetchall()
+        #     print(rows)
+        #     if not rows:
+        #         return jsonify({"error": sql_query}), 500
+        #     results = [
+        #         {description[0]: row[i] for i, description in enumerate(cursor.description)}for row in rows
+        #     ]
           
-            return jsonify(results)
+        #     return jsonify(results)
  
         finally:
             cursor.close()
